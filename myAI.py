2f5c497ea19dcbfef7a35fd3673ad563627e035b
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-21 10:43:39 +0000

    Binary search for depth.

--- myAI.py
+++ myAI.py
@@ -101,7 +101,7 @@ def tailIsReachable(state):
 
     stateCount = 1
 
-    while priorityQueue and stateCount <= 128:
+    while priorityQueue and stateCount <= 80:
 
         state, tail, _ = priorityQueue.popleft()
 
