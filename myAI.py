f3dcd4ce52fff1bed5034cb78e0db56b1afae9b6
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-21 22:29:46 +0000

    Final submission (unless jan changes copyGameState to not copy walls).

--- myAI.py
+++ myAI.py
@@ -52,7 +52,7 @@ def myAI(state: GameState) -> Turn:
         any(turnCounts[turn] for turn in Turn if turn != turnWhereTailIsNotReachable)
         if turnWhereTailIsNotReachable else
         len([turnCount for turnCount in turnCounts.values() if turnCount != 0]) >= 2
-    ) and stateCount <= 1536:
+    ) and stateCount <= 1024:
 
         state, turn, distance, _ = priorityQueue.popleft()
 
