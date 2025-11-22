430de2d09d470bc10a4c8d5678d9dc2c911d3ded
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-22 18:23:13 +0000

    Final commit.

--- myAI.py
+++ myAI.py
@@ -5,7 +5,7 @@ from snake.logic import DIRECTIONS
 from examples.smartAI import smartAI as enemyAI
 
 
-def myAI(state: GameState) -> Turn: #9
+def myAI(state: GameState) -> Turn:
 
     priorityQueue = deque()
 
