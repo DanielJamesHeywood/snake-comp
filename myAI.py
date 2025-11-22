556bd07a6e2ee7c972f7e9340aa1b7e86e5daa71
Daniel James Heywood <danieljamesheywood@icloud.com>
2025-11-22 18:20:53 +0000

    Insurance.

--- myAI.py
+++ myAI.py
@@ -5,7 +5,7 @@ from snake.logic import DIRECTIONS
 from examples.smartAI import smartAI as enemyAI
 
 
-def myAI(state: GameState) -> Turn: #2
+def myAI(state: GameState) -> Turn: #3
 
     priorityQueue = deque()
 
